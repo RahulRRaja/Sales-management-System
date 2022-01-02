98a8aa6afe69acf24028e07c2eae734eeb8c4d78
RahulRRaja <93483148+RahulRRaja@users.noreply.github.com>
2022-01-02 12:38:37 +0530

    Update Spare_PartsTypeRestController.java

--- Sales management System/Sales-management-System/src/main/java/com/Sample/Sales/management/System/restcontroller/Spare_PartsTypeRestController.java
+++ Sales management System/Sales-management-System/src/main/java/com/Sample/Sales/management/System/restcontroller/Spare_PartsTypeRestController.java
@@ -16,35 +16,35 @@ public class Spare_PartsTypeRestController {
     private Spare_Parts_TypesService spare_parts_typesService;
 
     @PostMapping("/add")
-    public Spare_Parts_types saveSpare_PartsTypes(@RequestBody Spare_Parts_types s)
+    public Spare_Parts_types saveSpare_PartsTypes(@RequestBody Spare_Parts_types s) throws InternalServerErrorException
     {
         spare_parts_typesService.CreateSparePartsTypes(s);
         return s;
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET, headers = "Accept=application/json")
-    public List<Spare_Parts_types> getAllSparePartsTypes()
+    public List<Spare_Parts_types> getAllSparePartsTypes() throws InternalServerErrorException
     {
         List<Spare_Parts_types> list = spare_parts_typesService.getAllSparePartsTypes();
         return list;
     }
     @DeleteMapping("/delete")
-    public void delete(Spare_Parts_types spare_parts_types)
+    public void delete(Spare_Parts_types spare_parts_types) throws InternalServerErrorException
     {
         spare_parts_typesService.deleteSparePartsTypes(spare_parts_types);
     }
     @DeleteMapping("/delete/{st_id}")
-    public void deleteById(@PathVariable("st_id") Integer st_id)
+    public void deleteById(@PathVariable("st_id") Integer st_id) throws InternalServerErrorException
     {
         spare_parts_typesService.deleteSparePartsTypesById(st_id);
     }
     @RequestMapping(value = "/get/{st_id}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public Spare_Parts_types getSparePartsTypeById(@PathVariable("st_id") Integer st_id)
+    public Spare_Parts_types getSparePartsTypeById(@PathVariable("st_id") Integer st_id) throws InternalServerErrorException
     {
         return  spare_parts_typesService.getSparePartsTypes(st_id);
     }
     @PutMapping("/edit")
-    public Spare_Parts_types editSpareParts(@RequestBody Spare_Parts_types s)
+    public Spare_Parts_types editSpareParts(@RequestBody Spare_Parts_types s) throws InternalServerErrorException
     {
         spare_parts_typesService.editSparePartsTypes(s);
         return s;
