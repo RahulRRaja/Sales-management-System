cdee1d1ace6de9f528ba1e0e97b17c6473a1c617
RahulRRaja <93483148+RahulRRaja@users.noreply.github.com>
2022-01-02 12:39:29 +0530

    Update UserRestController.java

--- Sales management System/Sales-management-System/src/main/java/com/Sample/Sales/management/System/restcontroller/UserRestController.java
+++ Sales management System/Sales-management-System/src/main/java/com/Sample/Sales/management/System/restcontroller/UserRestController.java
@@ -15,35 +15,35 @@ public class UserRestController {
     private UserService userService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, headers = "Accept=application/json")
-    public User saveUser(@RequestBody User u)
+    public User saveUser(@RequestBody User u) throws InternalServerErrorException
     {
          userService.CreateUser(u);
         return u;
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET, headers = "Accept=application/json")
-    public List<User> getAllProduct(@RequestParam(defaultValue = "0")Integer pageNo,@RequestParam(defaultValue = "20")Integer pageSize)
+    public List<User> getAllProduct(@RequestParam(defaultValue = "0")Integer pageNo,@RequestParam(defaultValue = "20")Integer pageSize) throws InternalServerErrorException
     {
         List<User> list = userService.getAllUser(pageNo,pageSize);
         return list;
     }
     @DeleteMapping("/delete")
-    public void delete(User user)
+    public void delete(User user) throws InternalServerErrorException
     {
         userService.deleteUser(user);
     }
     @DeleteMapping("/delete/{uid}")
-    public void deleteById(@PathVariable("uid") Integer uid)
+    public void deleteById(@PathVariable("uid") Integer uid) throws InternalServerErrorException
     {
         userService.deleteUserById(uid);
     }
     @RequestMapping(value = "/get/{uid}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public User getUserById(@PathVariable("uid") Integer uid)
+    public User getUserById(@PathVariable("uid") Integer uid) throws InternalServerErrorException
     {
       return  userService.getUser(uid);
     }
     @PutMapping("/edituser")
-    public User editUser(@RequestBody User u)
+    public User editUser(@RequestBody User u) throws InternalServerErrorException
     {
         userService.editUser(u);
         return u;
