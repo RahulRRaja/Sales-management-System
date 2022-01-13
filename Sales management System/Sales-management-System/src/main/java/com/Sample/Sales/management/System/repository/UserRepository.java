0e81591b5bf10796db4f07abb46b867ea72a5f6a
RahulRRaja <93483148+RahulRRaja@users.noreply.github.com>
2022-01-13 15:12:26 +0530

    Update UserRepository.java

--- Sales management System/Sales-management-System/src/main/java/com/Sample/Sales/management/System/repository/UserRepository.java
+++ Sales management System/Sales-management-System/src/main/java/com/Sample/Sales/management/System/repository/UserRepository.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<User,Integer> {
    @Query("SELECT u FROM user u WHERE u.user_name LIKE %:user_name%")
-    public List<User> getByUser(String user_name);
-  
+    public List<User> FindByUser(String user_name);
+    @Query("SELECT u FROM user u WHERE u.first_name LIKE %:first_name%")
+    public List<User> findByFirstName(String first_name);
+
+    @Query("SELECT u FROM user u WHERE u.last_name LIKE %:last_name%")
+    public List<User> findByLastName(String last_name);
+
 }
